16c2822a663ac506241b96b9eca085aad41d15c6
saliou33 <losaliou.sl33@gmail.com>
2023-04-26 10:03:21 +0000

    Add security services, modules config

---
+++ app/src/main/java/com/innov/workflow/app/config/IdmModule.java
@@ -0,0 +1,9 @@
+package com.innov.workflow.app.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.innov.workflow.idm"})
+public class IdmModule {
+}
---
+++ idm/src/main/java/com/innov/workflow/idm/config/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.innov.workflow.idm.config.service;
+
+import com.innov.workflow.core.domain.entity.User;
+import com.innov.workflow.core.domain.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
