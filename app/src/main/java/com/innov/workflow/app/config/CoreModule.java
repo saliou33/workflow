d92c6a2641f5234647f1f490625c4e0595188f74
saliou33 <losaliou.sl33@gmail.com>
2023-04-27 12:49:51 +0000

    Update core module config

--- app/src/main/java/com/innov/workflow/app/config/CoreModule.java
+++ app/src/main/java/com/innov/workflow/app/config/CoreModule.java
@@ -1,27 +1,15 @@
 package com.innov.workflow.app.config;
 
-import com.innov.workflow.core.domain.entity.User;
-import com.innov.workflow.core.domain.repository.UserRepository;
+
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.repository.query.FluentQuery;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 @Configuration
 @ComponentScan(basePackages = {"com.innov.workflow.core"})
 @EntityScan(basePackages = {"com.innov.workflow.core"})
 @EnableJpaRepositories(basePackages = {"com.innov.workflow.core"})
 public class CoreModule {
-
-
 }
