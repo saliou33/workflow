76d2ce7034f352251739bf7584d4072121c7f1b5
saliou33 <losaliou.sl33@gmail.com>
2023-05-10 13:40:12 +0000

    update Error Handling

--- app/src/main/java/com/innov/workflow/app/config/GlobalExceptionHandler.java
+++ app/src/main/java/com/innov/workflow/app/config/GlobalExceptionHandler.java
@@ -3,11 +3,13 @@ package com.innov.workflow.app.config;
 import com.innov.workflow.core.domain.ApiResponse;
 import com.innov.workflow.core.exception.ApiException;
 import io.jsonwebtoken.JwtException;
+import org.activiti.engine.ActivitiException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,7 +23,7 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
     public ResponseEntity handleValidationErrors(MethodArgumentNotValidException e) {
         List<String> errors = e.getBindingResult().getFieldErrors()
                 .stream().map(FieldError::getDefaultMessage)
@@ -34,6 +36,7 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(responseData, HttpStatus.BAD_REQUEST);
     }
 
+
     @ExceptionHandler(ApiException.class)
     public ResponseEntity handleApiException(ApiException e) {
 
@@ -42,13 +45,26 @@ public class GlobalExceptionHandler {
         responseData.put("code", e.getCode().name());
         if (e.getErrors() != null) {
             responseData.put("errors", e.getErrors());
-        } else {
-            List<String> errors = new ArrayList<String>();
-            errors.add(e.getMsg());
-            responseData.put("errors", errors);
         }
 
         return new ResponseEntity<>(responseData, e.getCode());
+    }
+
+    @ExceptionHandler({JwtException.class, ActivitiException.class})
+    public ResponseEntity handleCoreException(Exception e) {
+        ApiResponse responseData = new ApiResponse();
+        responseData.put("msg", e.getMessage());
+        responseData.put("code", HttpStatus.BAD_REQUEST.toString());
+        return new ResponseEntity(responseData, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleException(Exception e) {
+        e.printStackTrace();
+        ApiResponse responseData = new ApiResponse();
+        responseData.put("msg", "Server Error");
+        responseData.put("code", HttpStatus.BAD_REQUEST.toString());
+        return new ResponseEntity(responseData, HttpStatus.BAD_REQUEST);
     }
     
 }
