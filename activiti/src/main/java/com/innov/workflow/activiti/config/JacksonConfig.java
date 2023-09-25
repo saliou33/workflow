ab0c17f9e152e8f180383a55a78ab86d5b78a516
saliou33 <losaliou.sl33@gmail.com>
2023-09-25 13:59:42 +0000

    Add JodaModule for jackson date conversion in activiti

---
+++ activiti/src/main/java/com/innov/workflow/activiti/config/JacksonConfig.java
@@ -0,0 +1,16 @@
+package com.innov.workflow.activiti.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JodaModule());
+        return objectMapper;
+    }
+}
